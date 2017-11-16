494b5b947591960a16a70e077a784ea8a0b830c7
Nathalie Dos santos <nathds93370@gmail.com>
2017-11-16 09:52:29 +0100

    3eme commit

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 var app = (function init(){
 
 var tab = [];
-var Produit = function Produit(p) {
-  this.nom = p.nom;
-  this.prix = p.prix;
-  this.description = p.description;
+var Produit = function Produit(nom, prix, description) {
+  this.nom = nom;
+  this.prix = prix;
+  this.description = description;
 };
 
+
 var ajouterProduit = function() {
+
   var txt = document.getElementById("ajout_nom_prod").value;
   var txt2 = document.getElementById("ajout_prix_prod").value;
   var txt3 = document.getElementById("ajout_description_prod").value;
   document.getElementById("my_list").innerHTML += "<li>" + txt + "</li>";
   tab.push(txt, txt2, txt3);
-  console.log(tab);
 
 };
 
-return {
-  ajouterProduit: ajouterProduit
-};
+
+
+  return {
+    ajouterProduit: ajouterProduit
+  };
+
+
+
+
+
+  //
+  // function afficherProduitSup(){
+  //
+  //   document.getElementById("listesup").innerHTML +="<li>" + txt + "</li>";
+  //
+  // }
+
+
+  // etagere = new Produit ("Kallax", 35, "lkjkj");
+  // tapis = new Produit("Adum", 50, "JGjhguh");
+  // commode = new Produit("Malm" 125, "jkhkjhg");
+  // table = new Produit("Linnmon", 29.50, "hfgfhgf");
+
 
 }());
